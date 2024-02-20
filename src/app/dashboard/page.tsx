771eb0750ad375916c63a16e71cdc8ee0f65de1e
FearBeer <korus86@mail.ru>
2024-02-20 18:45:45 +0300

    card styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,24 @@
+import Card from "../ui/dashboard/card/card";
+import styles from "../ui/dashboard/dashboard.module.scss";
+
 function Dashboard() {
     return (
-        <div>Панель управления</div>
+        <div className={styles.wrapper}>
+            <div className={styles.cards}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            <div className={styles.transactions}>
+                transactions
+            </div>
+            <div className={styles.chart}>
+                chart
+            </div>
+            <div className={styles.rightbar}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt corporis pariatur voluptatem a. Quidem laborum sint sunt soluta rerum fugiat possimus maxime voluptas, iusto, doloribus ab perspiciatis nulla similique architecto!
+            </div>
+        </div>
     );
 }
 
