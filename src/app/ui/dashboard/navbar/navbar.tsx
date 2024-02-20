e8cdb489a3ab3e88bba09d3ff69b6b0a554df815
FearBeer <korus86@mail.ru>
2024-02-20 14:33:19 +0300

    added color vars in global for hover & active effects, changed navbar component

--- src/app/ui/dashboard/navbar/navbar.tsx
+++ src/app/ui/dashboard/navbar/navbar.tsx
@@ -1,6 +1,21 @@
+'use client'
+
+import { usePathname } from "next/navigation";
+import styles from "./navbar.module.scss";
+
 function Navbar() {
+    const pathname = usePathname();
     return (
-        <div>Панель навигации</div>
+        <div className={styles.container}>
+            <h3 className={styles.title}>
+                {pathname.split("/").pop()}
+            </h3>
+            <input
+                type="text"
+                placeholder="Найти..."
+                className={styles.serach}
+             />
+        </div>
     );
 }
 
